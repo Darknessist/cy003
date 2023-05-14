a71729887852f328f8ab79b8533c8822edcb3755
Darknessist <116794520+Darknessist@users.noreply.github.com>
2023-05-14 15:31:56 +0800

    Add files via upload

---
+++ components/my-search/my-search.vue
@@ -0,0 +1,71 @@
+<template>
+	<view>
+		<view class="my-search-container" :style="{'background-color':bgcolor}"
+		 @click="searchBoxHandler">
+			<view class="my-search-box" :style="{'border-radius':radius+'px'}">
+				<!-- 这是使用uni-ui提供的图标组件 -->
+				<image src="../../static/search.png"></image>
+				<text class="placeholder">搜索</text>
+			</view>
+			
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		name:"my-search",
+		props:{
+			//背景颜色
+			bgcolor:{
+				type:String,
+				default:'#C00000'
+			},
+			radius:{
+				type:Number,
+				default:18
+			}
+		},
+		data() {
+			return {
+				
+			};
+		},
+		methods:{
+			searchBoxHandler(){
+				console.log('ok');
+				this.$emit('click')
+			}
+		}
+	}
+</script>
+
+<style lang="scss">
+.my-search-container{
+	height: 50px;
+	// background-color: #C00000;
+	display: flex;
+	align-items: center;
+	padding: 0 10px;
+	.my-search-box{
+		height: 36px;
+		background-color: #fff;
+		// border-radius: 18px;
+		width: 100%;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		
+		   image{
+			width: 14px;
+			height: 14px;
+		
+		    }
+		.placeholder{
+			font-size: 15px;
+			margin-left: 5px;
+		}
+		
+	}
+}
+</style>
---
+++ subpkg/goods_list/goods_list.vue
@@ -0,0 +1,81 @@
+<template>
+	<view>
+		<view class="goods-list">
+			<view v-for="(goods,i) in goodsList" :key="i" @click="gotoDatail(goods)">
+			<my-goods :goods="goods"></my-goods>
+				
+			</view>
+			
+		</view>
+	</view>
+</template>
+
+<script>
+
+	export default {
+		data() {
+			return {
+				queryObj:{
+					query:'',
+					cid:'',
+					pagenum:1,
+					pagesize:10
+				},
+				goodsList:[],
+				total:0,
+				//节流阀
+				isloding:false
+				
+			};
+		},
+		onLoad(options) {
+			this.queryObj.query=options.query || ''
+			this.queryObj.cid=options.cid || ''
+			
+			this.getGoodsList()
+		},
+		methods:{
+			//获取商品列表数据的方法
+			async getGoodsList(cb){
+				//打开节流阀
+				this.isloding=true
+			 const {data:res} = await uni.$http.get('/api/public/v1/goods/search',this.queryObj)
+			 //关闭节流阀
+			 this.isloding=false
+			 cb && cb()
+			 if(res.meta.status !==200) return uni.$showMsg()
+			
+			 
+			 this.goodsList = [...this.goodsList,...res.message.goods]
+			 this.total=res.message.total
+			
+	
+			}, 
+			gotoDatail(goods){
+				uni.navigateTo({
+					url:'/subpkg/goods_detail/goods_detail?goods_id' + goods.goods_id
+				})
+			}
+		},
+		onReachBottom(){
+			if(this.queryObj.pagenum* this.queryObj.pagesize>=this.total)return uni.$showMsg('数据加载完毕')
+			if(this.isloding)return
+			//让页码值自增加1
+			this.queryObj.pagenum++
+			this.getGoodsList() 
+		},
+		onPullDownRefresh(){
+			//重置关键数据
+			this.queryObj.pagenum=1
+			this.total=0
+			this.isloding=false
+			this.goodsList=[]
+			
+			this.getGoodsList(()=>uni.stopPullDownRefresh())
+		}
+	}
+</script>
+
+<style lang="scss">
+
+</style>
