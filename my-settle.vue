0e4db398d501334bec44fd07b13a135a50396cde
Darknessist <116794520+Darknessist@users.noreply.github.com>
2023-05-14 15:31:08 +0800

    Add files via upload

---
+++ my-settle.vue
@@ -0,0 +1,20 @@
+<template>
+	<view>
+		我是sttle
+	</view>
+</template>
+
+<script>
+	export default {
+		name:"my-settle",
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style lang="scss">
+
+</style>
